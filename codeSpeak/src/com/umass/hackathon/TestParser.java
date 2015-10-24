ea32245bd9bf92e3c3bd2e895ce86b8ea5fae7d8
TheWooCardinal <pj.delmastro@yahoo.com>
2015-10-24 05:21:59 -0400

    many interfaces and new classes

--- codeSpeak/src/com/umass/hackathon/TestParser.java
+++ codeSpeak/src/com/umass/hackathon/TestParser.java
@@ -2,7 +2,7 @@ package com.umass.hackathon;
 
 public class TestParser {
 	public static void main(String[] args) {
-		Parser p = new Parser("data/test_parser.txt");
+		Parser p = new Parser("data/test_parser2.txt");
 		
 		for (String s = p.readSentence(); s != null; s = p.readSentence()) {
 			System.out.println(s);
