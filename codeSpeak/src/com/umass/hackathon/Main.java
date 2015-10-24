c8e58ed18ff767a7de47f437630bd6be2a93ac43
Roy <jackman.roy@gmail.com>
2015-10-24 07:20:22 -0400

    main implementation

--- codeSpeak/src/com/umass/hackathon/Main.java
+++ codeSpeak/src/com/umass/hackathon/Main.java
@@ -1,4 +1,5 @@
 package com.umass.hackathon;
+import java.util.Arrays;
 
 public class main{
 	public static void main(String[] args){
@@ -11,7 +12,29 @@ public class main{
 
 	void parse(String[] n){
 		if(n[0].toLowerCase().equals("within")){
-			
+			if(n[1].toLowerCase().equals("class")){
+				Class temp = new Class(n);
+				temp.name = n[2];
+				temp.components = Arrays.copyofRange(n,3,n.length());
+				temp.print();
+			} else {
+				Method temp = new Method();
+				temp.name = n[2];
+				temp.components = Arrays.copyofRange(n,3,n.length());
+				temp.print();
+			}
+		}else if(n[0].toLowerCase().equals("create")){
+			if(n[1].toLowerCase().equals("class")){
+				Class temp = new Class(n);
+				temp.name = n[2];
+				temp.components = Arrays.copyofRange(n,3,n.length());
+				temp.print();
+			} else if(n[1].toLowerCase().equals("method")){
+				Method temp = new Method();
+				temp.name = n[2];
+				temp.components = Arrays.copyofRange(n,3,n.length());
+				temp.print();
+			}
 		}
 	}
 }
